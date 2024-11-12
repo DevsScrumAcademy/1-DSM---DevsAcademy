555d0f845f35f29c143a0a91ee83e2ee4e4040e3
Arthur Augusto da Silva Cunha <130792219+ArthurAugusto10@users.noreply.github.com>
2024-11-11 21:53:09 -0300

    Add files via upload

--- Site Scrum/server/Server/src/controllers/UserController.js
+++ Site Scrum/server/Server/src/controllers/UserController.js
@@ -2,16 +2,27 @@ const client = require('./db');  // Importa a conexão do banco de dados
 
 // Função para inserir um usuário
 async function insertUser(req, res) {
-    const {nome, mail, senha} = req.body;
+    const {nome, mail, senha, proges, pergunta, resposta } = req.body;
 
     const query = {
-        text: 'INSERT INTO usuario (nome, mail, senha) VALUES ($1, $2, $3, $4, $5) RETURNING id, nome, mail',
-        values: [nome, mail, senha,]
+        text: 'INSERT INTO usuario (nome, mail, senha, progres, pergunta, resposta) VALUES ($1, $2, $3, $4, $5) RETURNING nome, mail, senha, proges, pergunta, resposta',
+        values: [nome, mail, senha, proges, pergunta, resposta ]
     };
+    
 
     try {
-        const result = await client.query(query);  // Executa a query
+        const queryDois = {
+            text: "Select mail from usuario where mail = $1" ,
+            values: [mail]
+        };
+        var result = await client.query(queryDois);
+        if (result.row.length > 0){
+            return "E-mail já existe!";
+        };
+
+        result = await client.query(query);  // Executa a query
         res.json(res.rows[0]);
+        return "Usuário cadastrado";
     } catch (err) {
         res.json({message: err.message});
     }
@@ -31,7 +42,7 @@ async function loginUser(req, res) {
         const result = await client.query(query);
 
         // Se o usuário não existir, retornamos um erro
-        if (result.rows.length === 0) {
+        if (result.rows.length == 0) {
             return res.status(400).json({ message: 'E-mail ou senha inválidos' });
         }
 
@@ -40,7 +51,9 @@ async function loginUser(req, res) {
         res.json({
             idusuario: user.idusuario,
             nome: user.nome,
-            mail: user.mail
+            mail: user.mail,
+            
+
         });
     } catch (err) {
         res.status(500).json({ message: err.message });
