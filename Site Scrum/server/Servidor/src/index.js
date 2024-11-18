50b2699c3943d7de48f630ddcda82ed0ac4ca287
Arthur Augusto da Silva Cunha <130792219+ArthurAugusto10@users.noreply.github.com>
2024-11-18 20:39:00 -0300

    Add files via upload
    Adicionando o servidor

---
+++ Site Scrum/server/Servidor/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+
+const user = require("./routes/user");
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, function(){
+    console.log(`Rodando na porta ${port}...`);
+});
+
+app.use("/user", user);
+
+// pega qualquer rota
+app.use("*", function(req,res){
+    res.send({error: "Requisição desconhecida"});
+});
